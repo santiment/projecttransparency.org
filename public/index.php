21b644b906c8b125da197aef849ced5cbbd02b35
Kim Carson <kim@brighteyelabs.com>
2017-10-02 12:26:59 -0700

    Somehow I missed adding Musiconomi and Maecenas to the array (but remember doing it before, so, not sure)

--- public/index.php
+++ public/index.php
@@ -40,7 +40,7 @@ $ethPrice = $priceResult['data']['amount'];
    NOTICE: The order of which they are displayed  equeals the order of the tickers given here
 */
 
-$tickers = array("ANT", "CFI", "DAP", "DNT", "DNA", "RSC", "GAT", "HSR", "PREB", "ICN", "IND", "LKK", "SAN", "VIC");
+$tickers = array("ANT", "CFI", "DAP", "DNT", "DNA", "RSC", "GAT", "HSR", "PREB", "ICN", "IND", "LKK", "ART", "MCI", "SAN", "VIC");
 
 /* get balances */
 
