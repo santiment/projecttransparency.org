4c629c82f62590c95341d7f00ac97b4dd3b41892
Kim Carson <kim@brighteyelabs.com>
2017-10-02 08:09:02 -0700

    t

--- populate.sql
+++ populate.sql
@@ -40,8 +40,8 @@ INSERT INTO project_eth_address (project_id, address)
       ('Maecenas', '0x02DC3b8AB87c562CdCE707647bd1ba21C390Faf4'),
       ('Musiconomi', '0xc7CD9d874F93F2409F39A95987b3E3C738313925'),
       ('Santiment', '0x6dd5a9f47cfbc44c04a0a4452f0ba792ebfbcc9a'),
-      ('Status.im', '0xA646E29877d52B9e2De457ECa09C724fF16D0a2B'),
-      ('Virgil Capital', '0x0D7096819e18D4B096998A1254Db8bbEC72c0c13')
+      ('Status.im', '0xA646E29877d52B9e2De457ECa09C724fF16D0a2B')
+      /* ('Virgil Capital', '0x0D7096819e18D4B096998A1254Db8bbEC72c0c13') */
     ) AS t (name, address)
   WHERE
     project.name = t.name
