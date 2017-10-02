a649100c17753b64cb4fe268e04ccdc1aabf7452
Kim Carson <kim@brighteyelabs.com>
2017-10-02 08:11:21 -0700

    Ok commenting the wallets isn't working, am removing to see what happens

--- populate.sql
+++ populate.sql
@@ -36,12 +36,10 @@ INSERT INTO project_eth_address (project_id, address)
       ('Encrypgen', '0x683a0aafa039406c104d814b9f244eea721445a7'),
       ('Etherisc', '0x9B0F6a5a667CB92aF0cd15DbE90E764e32f69e77'),
       ('Iconomi', '0x154Af3E01eC56Bc55fD585622E33E3dfb8a248d8'),
-      /* ('Indorse', '0x26967201d4d1e1aa97554838defa4fc4d010ff6f'), */
       ('Maecenas', '0x02DC3b8AB87c562CdCE707647bd1ba21C390Faf4'),
       ('Musiconomi', '0xc7CD9d874F93F2409F39A95987b3E3C738313925'),
       ('Santiment', '0x6dd5a9f47cfbc44c04a0a4452f0ba792ebfbcc9a'),
       ('Status.im', '0xA646E29877d52B9e2De457ECa09C724fF16D0a2B')
-      /* ('Virgil Capital', '0x0D7096819e18D4B096998A1254Db8bbEC72c0c13') */
     ) AS t (name, address)
   WHERE
     project.name = t.name
